caa636a66cbceefb69e25bc78515cd2befb1aead
AndyZheng <tianhua.zheng@theoutnet.cn>
2014-12-26 17:03:08 +0800

    mobile

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,9 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>后台管理</title>
 <link rel="icon" href="<?php echo $this->config->item( 'base_theme_url');?>images/fav1.ico" />
  <!-- Latest compiled and minified CSS -->
